3c7ea06d8a17f9adecf3dd87346be2e60fbda2e5
Roman <chuyanov2006@gmail.com>
2021-12-23 22:18:46 +0300

    update main.py and screensaver.py

--- data/screensaver.py
+++ data/screensaver.py
@@ -2,26 +2,26 @@ import cv2
 import pygame
 import os
 
-pygame.mixer.init()
-cap = cv2.VideoCapture(os.path.join('data', "aftforseabattle1366x768.mp4"))
-s = pygame.mixer.Sound(os.path.join('data', "aftforseabattle.wav"))
-success, img = cap.read()
-shape = img.shape[1::-1]
-wn = pygame.display.set_mode(shape)
-clock = pygame.time.Clock()
-s.play()
 
-while success:
-    clock.tick(60)
-    success, img = cap.read()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            success = False
-    try:
-        wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
-    except AttributeError:
-        pass
-    pygame.display.update()
-s.stop()
+def screensaver():
+    pygame.mixer.init()
+    cap = cv2.VideoCapture(os.path.join("data", "aftforseabattle1366x768.mp4"))
+    s = pygame.mixer.Sound(os.path.join("data", "aftforseabattle.wav"))
+    running, img = cap.read()
+    shape = img.shape[1::-1]
+    wn = pygame.display.set_mode(shape)
+    clock = pygame.time.Clock()
+    s.play()
 
-pygame.quit()
+    while running:
+        clock.tick(60)
+        running, img = cap.read()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        try:
+            wn.blit(pygame.image.frombuffer(img.tobytes(), shape, "BGR"), (0, 0))
+        except AttributeError:
+            pass
+        pygame.display.update()
+    s.stop()
---
+++ main.py
@@ -0,0 +1,12 @@
+from data.screensaver import screensaver
+import pygame
+
+
+def main():
+    pygame.init()
+    screensaver()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
