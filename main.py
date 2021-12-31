c0ecd45d7ca614014957957c5c3d7e334fcb73b8
Roman <chuyanov2006@gmail.com>
2021-12-31 11:27:37 +0300

    fix achievements

--- main.py
+++ main.py
@@ -24,20 +24,24 @@ def main():
             os.mkdir(f"{path}\Sea Battle")
             file.extractall(f"{path}\Sea Battle")
         else:
-            if not os.path.isfile(f"{path}\Sea Battle\config.txt"):
+            if not os.path.isfile(os.path.join(f"{path}\Sea Battle", 'config.txt')):
                 file.extract('config.txt', f"{path}\Sea Battle")
-            if not os.path.isfile(f"{path}\Sea Battle\achievements.csv"):
+            if not os.path.isfile(os.path.join(f"{path}\Sea Battle", 'achievements.csv')):
                 file.extract('achievements.csv', f"{path}\Sea Battle")
-            if not os.path.isfile(f"{path}\Sea Battle\statistic.txt"):
+            if not os.path.isfile(os.path.join(f"{path}\Sea Battle", 'statistic.txt')):
                 file.extract('statistic.txt', f"{path}\Sea Battle")
 
     pygame.init()  # инициализируем pygame
     screen, config, fps = create_window(f"{path}\Sea Battle")
-    settings, achievements = Settings(screen, fps, config, f"{path}\Sea Battle"), Achievements(
-        screen, fps, f"{path}\Sea Battle")
-    # pygame.mouse.set_visible(False)  # погашаем мышь
-    # menu.screensaver()  # заставка
-    # pygame.mouse.set_visible(True)  # показываем мышь
+    # menu, settings, achievements = Menu(
+    #     screen, fps, f"{path}\Sea Battle"), Settings(
+    #     screen, fps, config, f"{path}\Sea Battle"), Achievements(screen, fps, f"{path}\Sea Battle")
+    menu = Menu(screen, fps, f"{path}\Sea Battle")
+    settings = Settings(screen, fps, config, f"{path}\Sea Battle")
+    achievements = Achievements(screen, fps, f"{path}\Sea Battle")
+    pygame.mouse.set_visible(False)  # погашаем мышь
+    menu.screensaver()  # заставка
+    pygame.mouse.set_visible(True)  # показываем мышь
     achievements.add_progress(1, 1)
     while True:
         menu = Menu(screen, fps, f"{path}\Sea Battle")
