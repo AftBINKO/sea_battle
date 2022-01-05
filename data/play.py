a9a4d9565b0fdee5356fd5e5041f7429bd45e152
kamil <kamil.pro@bk.ru>
2022-01-05 13:54:23 +0300

    test

--- data/play.py
+++ data/play.py
@@ -1,16 +1,52 @@
-import pygame
+import pygame as pg
 
 
 class Board:
-    def __init__(self):
+    def __init__(self, screen):
         self.board = [[0] * 10 for _ in range(10)]
+        self.sc = screen
+        self.co = 30
+        self.size = 40
+        print()
 
+    def map_draw(self):
+        font = pg.font.Font(None, self.size // 2 + 10)
+        slo = list('АБВГДЕЁЖЗИ')
 
-class PlayWithFriend(Board):
-    pass
+        for i in range(10):
+            text = font.render(str(i), True, (255, 255, 255))
+            self.sc.blit(text, (i * self.size + self.co + 15, 3))
+
+        for i, pp in enumerate(slo):
+            print(i, pp)
+            text = font.render(pp, True, (255, 255, 255))
+            self.sc.blit(text, (0, i * self.size + self.co + 15))
+
+        for i in range(10):
+            for g in range(10):
+                pg.draw.rect(self.sc, (255, 255, 255), (i * self.size + self.co,
+                                                        g * self.size + self.co,
+                                                        self.size, self.size), 2)
 
 
 class PlayWithBot(Board):
+    def __init__(self, screen):
+        super().__init__(screen)
+        self.main()
+
+    def main(self):
+        running = True
+        while running:
+            self.sc.fill((0, 0, 0))
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+
+            self.map_draw()
+            pg.display.flip()
+
+
+class PlayWithFriend(Board):
     pass
 
 
