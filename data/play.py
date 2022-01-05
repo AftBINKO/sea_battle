b2a0d4e9dee787d22e34049773118d562f2ab77b
Roman <chuyanov2006@gmail.com>
2022-01-05 21:43:49 +0300

    added smoothness to the menu, added play menu

--- data/play.py
+++ data/play.py
@@ -1,17 +1,41 @@
 import pygame
 
+from data.main_functions import terminate, create_sprite
+
 
 class Board:
     pass
 
 
 class PlayWithFriend(Board):
-    pass
+    def __init__(self, screen, fps):
+        self.screen, self.size, self.fps = screen, screen.get_size(), fps
 
 
 class PlayWithBot(Board):
-    pass
+    def __init__(self, screen, fps):
+        self.screen, self.size, self.fps = screen, screen.get_size(), fps
 
 
 class Play(PlayWithBot):
-    pass
+    def menu(self):
+        clock = pygame.time.Clock()
+
+        while True:
+            menu_sprites = pygame.sprite.Group()
+
+            x = pygame.sprite.Sprite()
+            create_sprite(x, 'x.png', 1266, 50, menu_sprites)
+
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        terminate()
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        if x.rect.collidepoint(event.pos):
+                            return
+
+                self.screen.fill((0, 0, 0))
+                menu_sprites.draw(self.screen)
+                pygame.display.flip()
+                clock.tick(self.fps)
